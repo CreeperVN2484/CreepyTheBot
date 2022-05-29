0253bd83ba33bf7e2c621065565c89b5ebb881cc
CreeperVN2484 <minecraftgamemcgame2@gmail.com>
2022-05-29 22:42:35 +0700

    Fix for CBJS-63 (Unknown charactor)

--- commands/weather.js
+++ commands/weather.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
     const location = args[0];
 
     if (!location) {
-        return { error: "No Location provided" }
+        return message.channel.send("Error: No Location provided")
   }
     else {
         let content = await fetch(`https://pixel-api-production.up.railway.app/data/weather/?location=${location}`)
@@ -23,14 +23,18 @@ module.exports.run = async (client, message, args) => {
       if (content.error) {
           const say = new MessageEmbed()
               .setColor('0000FF')
+              .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+              .setThumbnail(client.user.displayAvatarURL())
                .setDescription("Failed to fetch weather infomations")
            return message.channel.send(say)
        }
        else {
            const say = new MessageEmbed()
                .setColor('0000FF')
-               .setDescription(`Info:\n\nLocation: ${content.info.location}\nCountry: ${content.info.country}\n\nWeather:Tempurature: ${content.weather.temp_c}°C / ${content.weather.temp_f}°F\nFeel Like: ${content.weather.feels_c}°C / ${content.weather.feels_f}°F\nCurrent Condition: ${content.weather.condition}\nWind: ${content.weather.wind_kph}KM/H / ${content.weather.wind_mph}MPH\nHumidity:${content.weather.humidity}%`)
-              .setImage("https://cdn.weatherapi.com/weather/64x64/day/113.png")
+               .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+               .setThumbnail(client.user.displayAvatarURL())
+               .setDescription(`Info:\n\nLocation: ${content.info.location}\nCountry: ${content.info.country}\n\nWeather:Tempurature: ${content.weather.temp_c}oC / ${content.weather.temp_f}oF\nFeel Like: ${content.weather.feels_c}oC / ${content.weather.feels_f}oF\nCurrent Condition: ${content.weather.condition}\nWind: ${content.weather.wind_kph}KM/H / ${content.weather.wind_mph}MPH\nHumidity:${content.weather.humidity}%`)
+              .setFooter(`$content.weather.icon`)
          return message.channel.send(say)
       }
 
