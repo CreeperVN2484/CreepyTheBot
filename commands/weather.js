ff81f9de76e15ad96cac397f5980f76f221144b4
CreeperVN2484 <minecraftgamemcgame2@gmail.com>
2022-05-29 20:58:01 +0700

    update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -4,58 +4,40 @@ const { MessageEmbed, Message, Client } = require("discord.js")
 module.exports.config = {
     name: "weather",
     aliases: ['wtl'],
-    cooldown: 100,
+    cooldown: 120,
     group: "misc",
     usage: '.weather <country>',
     description: "Shows a country's weather"
 }
 
-exports.Weather = async function (location) {
+module.exports.run = async (client, message, args) => {
+    const location = args[0];
+
     if (!location) {
         return { error: "No Location provided" }
-    }
+  }
     else {
         let content = await fetch(`https://pixel-api-production.up.railway.app/data/weather/?location=${location}`)
 
-        content = await content.json()
-        if (content.error) {
-            return content
-        }
-        else {
-            return content
-        }
+      content = await content.json()
+      if (content.error) {
+          const say = new MessageEmbed()
+              .setColor('0000FF')
+               .setDescription("Failed to fetch weather infomations")
+           return message.channel.send(say)
+       }
+       else {
+           const say = new MessageEmbed()
+              .setColor('0000FF')
+              .setDescription(content)
+         return message.channel.send(say)
+      }
 
     }
-}
 
-module.exports.run = async (client, message, args) => {
-  //  const location = args[1];
-
-//    if (!location) {
-//        return { error: "No Location provided" }
- //   }
- //   else {
- //       let content = await fetch(`https://pixel-api-production.up.railway.app/data/weather/?location=${location}`)
-
-  //      content = await content.json()
-  //      if (content.error) {
-  //          const say = new MessageEmbed()
-  //              .setColor('0000FF')
- //               .setDescription(content)
- //           message.channel.send(say)
- //       }
- //       else {
- //           const say = new MessageEmbed()
-  //              .setColor('0000FF')
-  //              .setDescription(content)
-  //          message.channel.send(say)
-  //      }
-
-   // }
-
-        const say = new MessageEmbed()
-            .setColor('0000FF')
-            .setDescription("Currently Disabled (API Broken)")
-        message.channel.send(say)
+//        const say = new MessageEmbed()
+//            .setColor('0000FF')
+//            .setDescription("Currently Disabled (API Broken)")
+//        message.channel.send(say)
 }
 
