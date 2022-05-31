22c4528c7e1372497e1109539468cfb000fd0da2
CreeperVN2484 <minecraftgamemcgame2@gmail.com>
2022-05-31 14:02:38 +0700

    test fix for CBJS-65 (command handler went wrong)

--- utils/handlers/commands.js
+++ utils/handlers/commands.js
@@ -3,38 +3,38 @@ const fs = require('fs')
 module.exports = (client) => {
     fs.readdir('./commands/', (err, files) => {
         if (err) console.log(err);
-    
+
         let jsFile = files.filter(f => f.split('.').pop() === 'js');
         if (jsFile.length <= 0) {
             return console.log('No commands')
         }
-    
+
         jsFile.forEach((file, i) => {
-            var pullCmd = require(`../../commands/${file}`);
-            client.commands.set(pullCmd.config.name, pullCmd);
+            let confcmd = require(`../../commands/${file}`);
+            client.commands.set(confcmd.config.name, confcmd);
 
-            console.log('Successfully loaded ' + pullCmd.config.name + ' command ', '✅')
+            console.log('Successfully loaded ' + confcmd.config.name + ' command ', '✅')
 
-            if (!pullCmd.config.name) {
+            if (!confcmd.config.name) {
                 console.log(`❌  -> missing a help.name, or help.name is not a string.`)
             }
-            if (!pullCmd.config.group) {
-                console.log('❌ -> Couldn\'t find any group in ' + pullCmd.config.name)
+            if (!confcmd.config.group) {
+                console.log('❌ -> Couldn\'t find any group in ' + confcmd.config.name)
                 return;
-        
+
             } else {
-                if (!client.groups.includes(pullCmd.config.group)) {
-                    return console.log('❌ -> Unknown group ' + `${pullCmd.config.group} in ` + pullCmd.config.name)
+                if (!client.groups.includes(confcmd.config.group)) {
+                    return console.log('❌ -> Unknown group ' + `${confcmd.config.group} in ` + confcmd.config.name)
                 }
             }
             try {
-            pullCmd.config.aliases.forEach(alias => {
-                client.aliases.set(alias, pullCmd.config.name);
-            });
-        } catch {
-            
-        }
-    
+                confcmd.config.aliases.forEach(alias => {
+                    client.aliases.set(alias, confcmd.config.name);
+                });
+            } catch {
+
+            }
+
         });
     });
 }
