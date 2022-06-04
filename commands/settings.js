11c4e3ae53939c8d586d1595d5c6a78084dfcbf4
CreeperVN2484 <minecraftgamemcgame2@gmail.com>
2022-06-04 13:46:24 +0700

    Removed Anti-bot / modlogs / clear database

--- commands/settings.js
+++ commands/settings.js
@@ -11,13 +11,6 @@ module.exports.config = {
 
 module.exports.run = async (client, message, args) => {
 
-    let modLogsChannel = 'None';
-
-    let modlogs = require('../database/modlogs.json')[message.guild.id];
-    if (modlogs) {
-        modLogsChannel = `<#${modlogs.channel}>`;
-    }
-
     let mRole = 'None';
 
     let muteRole = require('../database/muterole.json')[message.guild.id];
@@ -25,25 +18,10 @@ module.exports.run = async (client, message, args) => {
         mRole = `<@&${muteRole.role}>`
     }
 
-    let serverPrefix = client.prefix;
-
-    let anti_bot = '`Disabled`'
-
-    let antibot = require('../database/antibot.json')[message.guild.id];
-    if (antibot) {
-        anti_bot = `\`${antibot.func}\``
-    };
-
-
     const em = new MessageEmbed()
         .setColor("0000FF")
         .setDescription(`Configuration settings for **${message.guild.name}** \n \n `)
         .addFields(
-            {
-                name: 'Mod logs',
-                value: modLogsChannel
-                , inline: true
-            },
             {
                 name: 'Mute role',
                 value: mRole,
@@ -52,11 +30,6 @@ module.exports.run = async (client, message, args) => {
             {
                 name: 'Server prefix',
                 value: '`' + serverPrefix + '`',
-                inline: true
-            },
-            {
-                name: 'Anti bot',
-                value: anti_bot,
                 inline: true
             }
         )
